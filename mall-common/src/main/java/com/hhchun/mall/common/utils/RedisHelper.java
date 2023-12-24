3ca9ac75ea39da57845a9302b2677ffc28e3f07c
qqs <12487489@qq.com>
2023-12-24 19:00:55 +0800

    新增RedisHelper工具类

---
+++ mall-common/src/main/java/com/hhchun/mall/common/utils/RedisHelper.java
@@ -0,0 +1,27 @@
+package com.hhchun.mall.common.utils;
+
+import com.sun.istack.internal.NotNull;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RedisHelper {
+    /**
+     * scan实现
+     *
+     * @param redisTemplate redisTemplate
+     * @param pattern       表达式
+     */
+    public static List<String> scan(RedisTemplate<String, ?> redisTemplate, @NotNull String pattern) {
+        ScanOptions options = ScanOptions.scanOptions()
+                .match(pattern)
+                .count(2)
+                .build();
+        Cursor<String> cursor = redisTemplate.scan(options);
+        return cursor.stream().collect(Collectors.toList());
+    }
+}
