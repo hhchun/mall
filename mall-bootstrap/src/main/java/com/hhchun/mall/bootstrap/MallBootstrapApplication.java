88d26e3dc5008792d6fd79deb4507a0a8cbfb2e9
qqs <12487489@qq.com>
2024-01-04 05:19:02 +0800

    删掉没用的多余代码。

--- mall-bootstrap/src/main/java/com/hhchun/mall/bootstrap/MallBootstrapApplication.java
+++ mall-bootstrap/src/main/java/com/hhchun/mall/bootstrap/MallBootstrapApplication.java
@@ -1,18 +1,14 @@
 package com.hhchun.mall.bootstrap;
 
 import com.hhchun.mall.user.config.MallUserCoreConfiguration;
-import com.hhchun.mall.user.support.decision.AccessDecision;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(MallUserCoreConfiguration.class)
 public class MallBootstrapApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(MallBootstrapApplication.class);
-        AccessDecision accessDecision = context.getBean(AccessDecision.class);
-        System.out.println(accessDecision);
+       SpringApplication.run(MallBootstrapApplication.class);
     }
 }
